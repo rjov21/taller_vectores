57d0a0828070af26329b17aa5b5a605036b0d079
Robinson Ordoñez <robinsonor4@gmail.com>
2021-03-22 12:12:23 -0500

    parte a, punto 1

--- taller_vectores.py
+++ taller_vectores.py
@@ -4,4 +4,25 @@ Created on Sun Mar 21 16:57:54 2021
 
 @author: DIOS BENDITO
 """
+# ejercico1
+# parte a
 
+
+def sumatoria(numeros):
+    suma = 0
+    for valor in numeros:
+        suma = suma + valor
+    print(f'el valor de la sumatoria es: {suma}')
+
+
+i = 1
+numeros = []
+while True:
+    num = int(input(f'ingrese el numero {i} o ingrese 0 para finalizar: '))
+    if num == 0: break
+    numeros.append(num)
+    i += 1
+print(f'los numeros ingresados fueron {numeros}')
+
+
+sumatoria(numeros)
